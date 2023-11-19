b41afc4d7506daeff0fdb89cd432d1777ff6a994
Charlotte Wertz <charlotte.wertz@wsu.edu>
2023-11-19 10:45:58 -0800

    added community analysis, mapping

--- ml_simulation/estimate_surge.py
+++ ml_simulation/estimate_surge.py
@@ -1,28 +1,20 @@
-import numpy as np
 import pandas as pd
-import warnings
+import plotly.express as px
+import numpy as np
 
-warnings.filterwarnings(action='ignore', category=RuntimeWarning)
-from climada.hazard import TCTracks
-from climada.hazard import TropCyclone, Centroids
-
-# storm information
-data_provider = 'usa'  # set the data provider "usa" for default
-start_year = 1992  # start year in the range of years from which the hurricane data is required
-end_year = 2022  # end year in the range of years from which the hurricane data is required
-sub_basin = 'GM'  # can provide basin or sub-basin. Here, 'GM' refers to the gulf of mexico
-storm_id = '2017228N14314'  # For a specific hurricane
-time_report = 1  # set the hours of time stamps i.e. data is provided for every "time_report" hours
-
-# getting historic storm information from IBTrACS
-storm_track_all = TCTracks.from_ibtracs_netcdf(provider=data_provider, year_range=(start_year, end_year),
-                                               subbasin=sub_basin, correct_pres=False)
-
-# computing total number of historic storms
-storm_number_total = storm_track_all.size
-
-# getting the list of all storms
-storm_track_all_list = storm_track_all.data
-
-# getting storm id, name and category dataframe
-storm_info_df = pd.DataFrame(columns=['Storm ID', 'Name', 'Category'])
+hurricanes = pd.read_csv('ibtracs.NA.list.v04r00.csv')
+list_of_names = hurricanes['NAME']
+list_of_names = list_of_names.to_list()
+names = []
+[names.append(x) for x in list_of_names if x not in names]
+hurricanes = hurricanes.groupby(['SID'])
+Harvey = hurricanes.get_group('2017228N14314')
+# geometry = [Point(xy) for xy in zip(Harvey['LON'], Harvey['LAT'])]
+# gdf = GeoDataFrame(Harvey, geometry=geometry)
+flood = pd.read_csv('2017228N14314_surge/meow_Galveston.csv')
+fig = px.scatter_mapbox(Harvey,lat='LAT',lon='LON')
+fig.add_densitymapbox(lat=flood[' Lat'],lon=-1*flood[' Lon'])
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+print('done')
